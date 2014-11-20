a8083f631fe695e721dff8f7e5cc67a95b848511
mekagem <mekagem@gmail.com>
2014-11-20 12:47:40 +0300

    no .idea files here, +VoiceActivationScreen

--- src/com/github/subtitles/VoiceActivationScreen.java
+++ src/com/github/subtitles/VoiceActivationScreen.java
@@ -3,7 +3,7 @@ package com.github.subtitles;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
+public class VoiceActivationScreen extends Activity {
     /**
      * Called when the activity is first created.
      */
